33d3bfe7e3c9bf84cd18f47c5140af93cd7975cd
Brunão <140030397+brunao23@users.noreply.github.com>
2024-10-30 02:19:40 -0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,20 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+console.log('index.js is being executed');
+
+try {
+  console.log('Attempting to render React app');
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  console.log('React app rendered successfully');
+} catch (error) {
+  console.error('Error rendering React app:', error);
+}
